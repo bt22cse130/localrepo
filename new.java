054c02ea1554980ffaf3342cb0a71530dc03901b
bt22cse130 <bt22cse130.ac.in>
2023-11-06 19:30:59 +0530

    input from User

--- new.java
+++ new.java
@@ -1,7 +1,10 @@
+import java.util.*;
 public class New
 {
     public static void main(String args[])
     {
        System.out.println("Hello World");
+       Scanner sc= new Scanner(System.in);
+       int n=sc.nextInt();
     }
 }
